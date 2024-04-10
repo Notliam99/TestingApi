25b082332689e1b9b5f22381254c6b75b6f8fd28
Liam Tietjens <l.tietjens@hotmail.com>
2024-04-10 15:07:43 +1200

    fixed and added a json path

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use serde::Serialize;
 
-#[get("/zach")]
+#[get("/hello/plain/{name}")]
 async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello monkey!")
+    format!("Hello {name} Your A Monkey! 🦍")
+}
+
+#[derive(Serialize)]
+struct Message {
+    hello: String,
+}
+
+#[get("/hello/json/{name}")]
+async fn json_hello(name: web::Path<String>) -> Result<web::Json<Message>> {
+    let message = Message {
+        hello: format!("Hello {} Your A Super Monkey! 🙊", name.to_string()),
+    };
+
+    Ok(web::Json(message))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(json_hello))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
