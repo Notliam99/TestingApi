57f6a5f3455f6a5b64af30192716b527406aef9d
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-04-10 13:34:26 +1200

    Test - Test
    lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/zach")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello monkey!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
