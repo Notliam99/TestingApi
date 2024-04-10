f11b712b672236ca32a6267a5eeffb6b50e220ae
Ellozac <zlsy108@gmail.com>
2024-04-10 18:21:22 +1200

    added a flag to specify custom port

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,33 @@ async fn hello(name: web::Path<String>) -> impl Responder {
     format!("Hello monkey!")
 }
 
+async fn check_port() -> u16 {
+    let args: Vec<String> = std::env::args().collect();
+    // iterate through arguements checking for port arguement
+    for (_index, arg) in args.iter().enumerate() {
+        if arg == "-p" || arg == "--port" {
+            // check if a arguement exists after -p
+            if let Some(port_str) = args.get(_index + 1) {
+                /* if string converts to unsigned 16 int then return the port
+                otherwise error etc. default to 8080 */
+                if let Ok(port) = port_str.parse::<u16>() {
+                    println!("Port set to {}", port);
+                    return port;
+                } else {
+                    println!("No valid port specified defaulting to 8080");
+                    return 8080;
+                }
+            } 
+        }
+    }
+    // if there are no arguments return 8080
+    return 8080;
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = check_port().await;
     HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", port))?
         .run()
         .await
 }
