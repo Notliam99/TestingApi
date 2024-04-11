9e6160146821aad4ff28f1002ac9e23d3484624f
Liam Tietjens <l.tietjens@hotmail.com>
2024-04-12 09:18:50 +1200

    Reworked most of the code into a library

---
+++ src/args/mod.rs
@@ -0,0 +1,24 @@
+// this function checks for the -p or --port flags and returns a unsigned 16 int aka the arg
+pub fn check_port() -> u16 {
+    let default_port: u16 = 8080;
+    let args: Vec<String> = std::env::args().collect();
+    // iterate through arguements checking for port arguement
+    for (index, arg) in args.iter().enumerate() {
+        if arg == "-p" || arg == "--port" {
+            // check if a arguement exists after -p
+            if let Some(port_str) = args.get(index + 1) {
+                /* if string converts to unsigned 16 int then return the port
+                otherwise error etc. default to default_port */
+                if let Ok(port) = port_str.parse::<u16>() {
+                    println!("Port set to {}", port);
+                    return port;
+                } else {
+                    println!("No valid port specified defaulting to {}", default_port);
+                    return default_port;
+                }
+            }
+        }
+    }
+    // if there are no arguments return default port
+    return default_port;
+}
