d0c7236f78d7f5bfe2e6a4b1f5b234de30a1e6fc
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-04-12 13:42:56 +1200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ async fn main() {
     // init the logger
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    // Port Helper
-    let port = args::check_port();
-
-    // Start the api server
+    // Start the api server with port helper (args::check_port())
     HttpServer::new(|| {
         App::new()
             .service(paths::hello)
@@ -17,7 +14,7 @@ async fn main() {
             .service(paths::qparams_hello)
             .wrap(Logger::new("Ip: ( %a ), Path: ( %U ), Latency: ( %Dms )")) // Logging
     })
-    .bind(("127.0.0.1", port))
+    .bind(("127.0.0.1", args::check_port()))
     .unwrap()
     .run()
     .await
