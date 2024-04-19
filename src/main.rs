e2e2ffb08b0cc08714074039ffc3b032877112e5
Liam Tietjens <l.tietjens@hotmail.com>
2024-04-19 16:39:52 +1200

    [Fix] allow all origins

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
             .service(paths::qparams_hello)
             .wrap(Logger::new("Ip: ( %a ), Path: ( %U ), Latency: ( %Dms )")) // Logging
     })
-    .bind(("127.0.0.1", args::check_port()))
+    .bind(("0.0.0.0", args::check_port()))
     .unwrap()
     .run()
     .await
